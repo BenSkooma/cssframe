55f3d2c06f006dcbe85232f2f987daa46cc6e6ed
BenSkooma <benjamin.launicke@googlemail.com>
2022-07-14 20:36:10 +0200

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
-/* Feature Detection : js,touch
+/* Feature Detection
 /* ---------------------------------- */
 !function(a,b){"use strict";b.documentElement.className=b.documentElement.className.replace("no-js","js"),("ontouchstart"in window||window.DocumentTouch&&document instanceof DocumentTouch)&&(document.documentElement.className=document.documentElement.className.replace("no-touch","touch"))}(window,document);
+
+!function(result, html){"use strict";if(!result || result === 'none') addClass(html, 'no-where')} (window.getComputedStyle(document.documentElement,':before').content, document.documentElement);
 
 /* CSS-Vars-Ponyfill Config
 /* ---------------------------------- */
@@ -18,3 +20,15 @@ cssVars({
   updateURLs    : true,
   watch         : false
 });
+
+/* Add Class Function
+/* ---------------------------------- */
+
+function hasClass(el, className) {
+  if (el.classList) return el.classList.contains(className)
+  else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+  }
+  function addClass(el, className) {
+  if (el.classList) el.classList.add(className)
+  else if (!hasClass(el, className)) el.className += " " + className
+  }

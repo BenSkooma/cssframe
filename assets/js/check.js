72aad91585cde84ee823737a3847a7b9b7d22ead
BenSkooma <benjamin.launicke@googlemail.com>
2022-07-19 14:41:30 +0200

    Update check.js

--- assets/js/check.js
+++ assets/js/check.js
@@ -1,18 +1,12 @@
 var log = true;
 
-var conditional = document.getElementById("conditional");
 var fallback_css = 'assets/css/reset.legacy.js.css';
+var conditional = document.getElementById("conditional");
 var supports = checkRule('@supports', log);
 var where = checkSelector(':where(*)', log);
 var applied = cssApplied(document.documentElement);
 
-if (!supports || !where && !applied) setFallback();
-// else if (!where && !applied) setFallback();
-
-function setFallback() { conditional.setAttribute('href', fallback_css); }
-
-
-
+if (!supports || !where && !applied) conditional.setAttribute('href', fallback_css);
 
 function cssApplied(element) {
   var content = window.getComputedStyle(element, '::before').content;
@@ -20,6 +14,41 @@ function cssApplied(element) {
   return true;
 }
 
+function checkRule(value, log) {
+  var rule = value;
+  var rule_prefixes = ['MOZ_', 'WEBKIT_', 'O_', 'MS_', ''];
+  var support = false;
+  var length = rule_prefixes.length;
+  rule = rule.replace(/^@/, '').toUpperCase().split('-').join('_') + '_RULE';
+  while (!support && length--) support = (rule_prefixes[length] + rule) in CSSRule;
+  if (log) console.log(value, '=',  support);
+  return support;
+}
+
+function checkSelector(selector, log) {
+  if (window.CSS && CSS.supports) return api(selector);
+  var head = document.head || document.documentElement;
+  var style = document.createElement('style');
+  head.insertBefore(style, head.firstChild);
+  var sheet = style.sheet;
+  var result = append(selector);
+  if (log) console.log(selector, '=', result);
+  return result;
+  function api(selector) {
+    var support = CSS.supports('selector('+ selector +')');
+    if (log) console.log('API', selector, '=', support);
+    return support;
+  }
+  function append(selector) {
+    try {
+      sheet.insertRule(selector + '{}', 0);
+      sheet.deleteRule(0);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
 
 
 
@@ -70,117 +99,118 @@ function cssApplied(element) {
 
 /* Feature test CSS Property and value support
 /*----------------------------------------------------*/
-function checkStyle(prop, value) {
+// function checkStyle(prop, value) {
 
-  value = arguments.length === 2 ? value : 'inherit';
+//   value = arguments.length === 2 ? value : 'inherit';
   
-  // var support = api(prop, value);
-  // if (support) return true;
+//   // var support = api(prop, value);
+//   // if (support) return true;
 
-  if (window.CSS && CSS.supports) return api(prop, value);
+//   if (window.CSS && CSS.supports) return api(prop, value);
 
-  var dummy = document.createElement('div');
-  var prefixes = ['Webkit', 'Moz', 'O', 'ms'];
-  var length = prefixes.length;
+//   var dummy = document.createElement('div');
+//   var prefixes = ['Webkit', 'Moz', 'O', 'ms'];
+//   var length = prefixes.length;
 
-  var camel = toCamelCase(prop);
-  var capitalized = camel.charAt(0).toUpperCase() + camel.slice(1);
+//   var camel = toCamelCase(prop);
+//   var capitalized = camel.charAt(0).toUpperCase() + camel.slice(1);
 
-  support = canSetProperty(prop, camel, value);
+//   support = canSetProperty(prop, camel, value);
 
-  while (!support && length--) {
-    var prefixed = '-' + prefixes[length].toLowerCase() + '-' + prop;
-    support = api(prefixed, value);
-    if (!support) {
-      camel = prefixes[length] + capitalized;
-      support = canSetProperty(prefixed, camel, value);
-    }
-  }
+//   while (!support && length--) {
+//     var prefixed = '-' + prefixes[length].toLowerCase() + '-' + prop;
+//     support = api(prefixed, value);
+//     if (!support) {
+//       camel = prefixes[length] + capitalized;
+//       support = canSetProperty(prefixed, camel, value);
+//     }
+//   }
 
-  console.log(prop, ':', value, '=', support);
-  return support;
+//   console.log(prop, ':', value, '=', support);
+//   return support;
 
-  /* ---------------------------------- */
+//   /* ---------------------------------- */
 
-  function api(prop, value) {
-    var result = CSS.supports(prop, value);
-    if (value === 'inherit') {
-      console.log('API', prop, '=', result);
-    } else {
-      console.log('API', prop, ':', value, '=', result);
-    }
-    return result;
-  }
+//   function api(prop, value) {
+//     var result = CSS.supports(prop, value);
+//     if (value === 'inherit') {
+//       console.log('API', prop, '=', result);
+//     } else {
+//       console.log('API', prop, ':', value, '=', result);
+//     }
+//     return result;
+//   }
 
-  function toCamelCase(str) {
-    return str.toLowerCase().replace(/-([a-z]|[0-9])/ig, function(match, chr) { return chr.toUpperCase(); });
-  }
+//   function toCamelCase(str) {
+//     return str.toLowerCase().replace(/-([a-z]|[0-9])/ig, function(match, chr) { return chr.toUpperCase(); });
+//   }
 
-  function canSetProperty(prop, camel, value) {
-    var support = camel in dummy.style;
-    if (value === 'inherit') return support;
-    dummy.style.cssText = prop + ':' + value;
-    return support && dummy.style[camel] !== '';
-  }
+//   function canSetProperty(prop, camel, value) {
+//     var support = camel in dummy.style;
+//     if (value === 'inherit') return support;
+//     dummy.style.cssText = prop + ':' + value;
+//     return support && dummy.style[camel] !== '';
+//   }
   
-}
+// }
 
 
 
 /* Feature test CSS at-rule support
 /*----------------------------------------------------*/
-function checkRule(value, log) {
-  var rule = value;
-  var rule_prefixes = ['MOZ_', 'WEBKIT_', 'O_', 'MS_', ''];
-  var support = false;
-  var length = rule_prefixes.length;
-  rule = rule.replace(/^@/, '').toUpperCase().split('-').join('_') + '_RULE';
-  while (!support && length--) support = (rule_prefixes[length] + rule) in CSSRule;
-  if (log) console.log(value, '=',  support);
-  return support;
-}
+// function checkRule(value, log) {
+//   var rule = value;
+//   var rule_prefixes = ['MOZ_', 'WEBKIT_', 'O_', 'MS_', ''];
+//   var support = false;
+//   var length = rule_prefixes.length;
+//   rule = rule.replace(/^@/, '').toUpperCase().split('-').join('_') + '_RULE';
+//   while (!support && length--) support = (rule_prefixes[length] + rule) in CSSRule;
+//   if (log) console.log(value, '=',  support);
+//   return support;
+// }
 
 /* Feature test CSS.API @supports selector() support
 /*----------------------------------------------------*/
-function checkSupportsSelector(log) {
-  if (window.CSS && CSS.supports) {
-    var result = CSS.supports('selector(*)');
-    if (log) console.log('API', '@supports selector(*) =', result);
-    return result;
-  } else { return false; }
-}
+// function checkSupportsSelector(log) {
+
+//   if (window.CSS && CSS.supports) {
+//     var result = CSS.supports('selector(*)');
+//     if (log) console.log('API', '@supports selector(*) =', result);
+//     return result;
+//   } else { return false; }
+// }
 
 /* Feature test CSS selector support
 /*----------------------------------------------------*/
-function checkSelector(selector, log) {
+// function checkSelector(selector, log) {
 
-  if (window.CSS && CSS.supports) return api(selector);
+//   if (window.CSS && CSS.supports) return api(selector);
 
-  var head = document.head || document.documentElement;
-  var style = document.createElement('style');
+//   var head = document.head || document.documentElement;
+//   var style = document.createElement('style');
 
-  head.insertBefore(style, head.firstChild);
+//   head.insertBefore(style, head.firstChild);
 
-  var sheet = style.sheet;
-  var result = append(selector);
+//   var sheet = style.sheet;
+//   var result = append(selector);
 
-  if (log) console.log(selector, '=', result);
+//   if (log) console.log(selector, '=', result);
 
-  return result;
+//   return result;
 
-  function api(selector) {
-    var support = CSS.supports('selector('+ selector +')');
-    if (log) console.log('API', selector, '=', support);
-    return support;
-  }
+//   function api(selector) {
+//     var support = CSS.supports('selector('+ selector +')');
+//     if (log) console.log('API', selector, '=', support);
+//     return support;
+//   }
 
-  function append(selector) {
-    try {
-      sheet.insertRule(selector + '{}', 0);
-      sheet.deleteRule(0);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-}
+//   function append(selector) {
+//     try {
+//       sheet.insertRule(selector + '{}', 0);
+//       sheet.deleteRule(0);
+//       return true;
+//     } catch (e) {
+//       return false;
+//     }
+//   }
+// }

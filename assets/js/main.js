d7e7dc8c0f6a5f8e471e986127d75e03a041f9b5
BenSkooma <benjamin.launicke@googlemail.com>
2022-07-22 18:01:45 +0200

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@
 
 !function(d){"use strict";d.documentElement.className=d.documentElement.className.replace("no-js","js"),("ontouchstart"in window||window.DocumentTouch&&document instanceof DocumentTouch)&&(document.documentElement.className=document.documentElement.className.replace("no-touch","touch"))}(document);
 
+// checkSupportsNot(document.documentElement);
+
 /* ---------------------------------- */
 
 cssVars({
@@ -21,3 +23,28 @@ cssVars({
 });
 
 /* ---------------------------------- */
+
+// function checkSupportsNot(html) {
+
+//   if(!cssApplied(html)) addClass(html, 'no-supports-not');
+
+//   function cssApplied(element) {
+//     var content = window.getComputedStyle(element, '::before').content;
+//     if (!content || content === 'none') return false;
+//     return true;
+//   }
+
+//   function hasClass(el, className) {
+//     if (el.classList)
+//       return el.classList.contains(className)
+//     else
+//       return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+//   }
+  
+//   function addClass(el, className) {
+//     if (el.classList)
+//       el.classList.add(className)
+//     else if (!hasClass(el, className)) el.className += " " + className
+//   }
+
+// }
